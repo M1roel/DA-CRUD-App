da1daae96da43db9191e3691ad914b3d8ff2f8aa
M1roel <ppfautsch@yahoo.de>
2024-05-26 17:53:10 +0200

    Loeschen Button und Funktion hinzugefuegt

--- script.js
+++ script.js
@@ -1,20 +1,44 @@
-let names = ['Erica Mustermann', 'John Doe'];
-let numbers = ['02161123456', '02161456789'];
+let names = ["Erica Mustermann", "John Doe"];
+let numbers = ["02161123456", "02161456789"];
 
+
+function addContact() {
+    let name = document.getElementById('name');
+    let phone = document.getElementById('phone');
+    names.push(name.value);
+    numbers.push(phone.value);
+
+    render();
+}
+
+function deleteContact(i) {
+    names.splice(i, 1);
+    numbers.splice(i, 1);
+
+    render();
+}
 
 function render() {
-    let content = document.getElementById('content');
-    content.innerHTML = '';
-    content.innerHTML += `<h1>My Contacts</h1>`;
+  let content = document.getElementById("content");
+  content.innerHTML = "";
+  content.innerHTML += `<h1>My Contacts</h1>`;
+  content.innerHTML += `
+    <div>
+        <input placeholder="Name" id="name">
+        <input placeholder="Telefon" id="phone">
+        <button onclick="addContact()">Hinzufügen</button>
+    </div>
+    `;
 
-    for (let i = 0; i < names.length; i++) {
-        const name = names[i];
-        const number = numbers[i];
-        content.innerHTML += `
+  for (let i = 0; i < names.length; i++) {
+    const name = names[i];
+    const number = numbers[i];
+    content.innerHTML += `
             <div class="card">
                 <b>Name: </b> ${name} <br>
                 <b>Telefonnummer: </b> ${number} <br>
+                <button onclick="deleteContact(${i})">Löschen</button>
             </div>
-        `;       
-    }
+        `;
+  }
 }
