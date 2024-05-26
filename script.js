86a1f044afabd0f333d9c4b5f8bc1030283f49fd
M1roel <ppfautsch@yahoo.de>
2024-05-26 15:52:27 +0200

    Array fuer Kontakte und FOR-Schleife um diese anzuzeigen hinzugefuegt

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
+let names = ['Erica Mustermann', 'John Doe'];
+let numbers = ['02161123456', '02161456789'];
+
+
 function render() {
-    let contend = document.getElementById('content');
-    contend.innerHTML = '';
-    contend.innerHTML += `<h1>My Contacts</h1>`;
+    let content = document.getElementById('content');
+    content.innerHTML = '';
+    content.innerHTML += `<h1>My Contacts</h1>`;
+
+    for (let i = 0; i < names.length; i++) {
+        const name = names[i];
+        const number = numbers[i];
+        content.innerHTML += `
+            <div>
+                <b>Name: </b> ${name} <br>
+                <b>Telefonnummer: </b> ${number} <br>
+            </div>
+        `;       
+    }
 }
